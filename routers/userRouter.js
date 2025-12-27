5b9f9b2b7beb07910c3dec78087d98b7c775a7b0
Himeshas <sandaruwan519@gmail.com>
2025-12-27 22:39:08 +0530

    order controller modified

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createUsers, loginUser } from "../controllers/userController.js";
+import { createUsers, getUser, loginUser } from "../controllers/userController.js";
 
 const userRouter = express.Router()
 
 userRouter.post(
     "/",createUsers
 );
+
+userRouter.get("/",getUser)
 
 userRouter.post("/login",loginUser)
 
